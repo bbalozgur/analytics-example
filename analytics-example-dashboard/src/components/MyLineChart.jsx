0203b1f58bbabb4ec27fb53e4621dc33becf36ab
bbalozgur <balozgur94@gmail.com>
2020-10-18 22:33:37 +0300

    add/line-chart-test

--- analytics-example-dashboard/src/components/MyLineChart.jsx
+++ analytics-example-dashboard/src/components/MyLineChart.jsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { LineChart, XAxis, Tooltip, CartesianGrid, Line, YAxis} from 'recharts';
+import {
+  LineChart,
+  XAxis,
+  Tooltip,
+  CartesianGrid,
+  Line,
+  YAxis,
+} from "recharts";
 
-const MyLineChart = ({ data, width, height }) => {
-  if (!data || data.length === 0) return <div>data yok</div>
+const MyLineChart = ({ data, width, height, testId }) => {
+  if (!data || data.length === 0) return <div>data yok</div>;
   return (
-    <LineChart
-      width={width}
-      height={height}
-      data={data}
-      margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
-    >
-      <XAxis dataKey="created_at" />
-      <YAxis dataKey="value" />
-      <Tooltip />
-      <CartesianGrid stroke="#f5f5f5" />
-      <Line type="monotone" dataKey="value" stroke="#ff7300" yAxisId={0} />
-    </LineChart>
+    <div className="line-chart" data-testid={`line-chart-${testId}`}>
+      <LineChart
+        width={width}
+        height={height}
+        data={data}
+        margin={{ top: 5, right: 20, left: 10, bottom: 5 }}
+      >
+        <XAxis dataKey="created_at" />
+        <YAxis dataKey="value" />
+        <Tooltip />
+        <CartesianGrid stroke="#f5f5f5" />
+        <Line type="monotone" dataKey="value" stroke="#ff7300" yAxisId={0} />
+      </LineChart>
+    </div>
   );
+};
+
+MyLineChart.defaultProps = {
+  testId: 'test',
 };
 
 export default MyLineChart;
---
+++ analytics-example-dashboard/src/components/MyLineChart.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import MyLineChart from './MyLineChart';
+
+afterEach(cleanup);
+
+const data = [
+    {_id: '5f8c89665d349c63142cd484', created_at: '2020-10-18T18:28:56.281Z', value: '5', __v: 0},
+    {_id: '5f8c89665d349c63142cd484', created_at: '2020-10-18T18:28:56.281Z', value: '5', __v: 0},
+    {_id: '5f8c89665d349c63142cd484', created_at: '2020-10-18T18:28:56.281Z', value: '5', __v: 0},
+    {_id: '5f8c89665d349c63142cd484', created_at: '2020-10-18T18:28:56.281Z', value: '5', __v: 0}
+];
+
+describe('Checkbox component', () => {
+  it('should be render with name', () => {
+    render(
+      <MyLineChart />,
+    );
+  });
+
+  it('should be render not props to data', () => {
+    render(<MyLineChart />);
+    const lineChartElement = document.querySelector('.line-chart');
+
+    expect(lineChartElement).toBe(null);
+  });
+
+  it('should be render props to data', () => {
+    render(<MyLineChart  data={data} />);
+    const lineChartElement = document.querySelector('.line-chart');
+
+    expect(lineChartElement.className.includes('line-chart')).toBe(true);
+  });
+});
